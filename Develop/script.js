00d7ac41b1a10eb67d82596d2c9b163d7adc0c21
kcbryan <kcbryan10@gmail.com>
2021-10-10 22:15:24 -0500

    add functions to generate random password/ updated letter arrays to be split

--- Develop/script.js
+++ Develop/script.js
@@ -1,49 +1,71 @@
 // Assignment code here
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "<", ">", "(", ")", "?", "=", "[", "]"]
-var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '/',]
+var lower = "qwertyuiopasdfghjklzxcvbnm".split('');
+var upper = "QWERTYUIOPASDFGHJKLZXCVBNM".split('');
+var number = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '?', '/',]
+var PasswordCriteria= [];
 
 function GetCriteria() {
-  var userInput = prompt("How long would you like you password to be?")
+  var input = prompt("How long would you like you password to be?")
+
+  var PasswordLength = parseInt(input);
+
 
   if (PasswordLength < 8) {
-    alert("Password must be between 8-128 characters")
+    alert("Password must be between 8-128 characters.")
     return GetCriteria();
   }
 
   if (PasswordLength > 128) {
-    alert("Password must be between 8-128 characters")
+    alert("Password must be between 8-128 characters.")
     return GetCriteria();
   }
 
-  if (window.confirm("Do you want lower case letters in you password?")) {
+  if (confirm("Do you want lower case letters in you password?")) {
     PasswordCriteria = PasswordCriteria.concat(lower);
   }
 
-  if (window.confirm("Do you want upper case letters in you password?")) {
+  if (confirm("Do you want upper case letters in you password?")) {
     PasswordCriteria = PasswordCriteria.concat(upper);
   }
 
-  if (window.confirm("Do you want special character in you password?")) {
+  if (confirm("Do you want special character in you password?")) {
     PasswordCriteria = PasswordCriteria.concat(special);
   }
 
-  if (window.confirm("Do you want numbers in you password?")) {
+  if (confirm("Do you want numbers in you password?")) {
     PasswordCriteria = PasswordCriteria.concat(number);
   }
 
   if (PasswordCriteria.length === 0) {
-    alert("Please choose at least one Character Choice");
+    alert("Choose at least one character choice");
     return GetCriteria();
   }
 
   var options = {
     PasswordLength,
     PasswordCriteria
-  }
+  };
 
   return options;
+}
+
+function GenRandom(){
+  var random = Math.floor(Math.random()* PasswordCriteria.length)
+  return random
+}
+
+
+function GeneratePassword(){
+  var options = GetCriteria();
+  var ResultingPassword= []
+
+  for(var i= 0; i < options.PasswordLength; i++){
+    var digit = options.PasswordCriteria[GenRandom()];
+    ResultingPassword.push(digit);
+  }
+
+  return ResultingPassword.join("");
 }
 
 var generateBtn = document.querySelector("#generate");
@@ -52,9 +74,7 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = GeneratePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
